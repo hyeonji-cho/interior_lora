7c0edd28f9294e2b10b923ab5b00ac8fe9a9072b
hyeonji-cho <89562865+hyeonji-cho@users.noreply.github.com>
2025-09-03 17:44:05 +0900

    Update training script parameters for model training

--- adalora/train_sdxl.sh
+++ adalora/train_sdxl.sh
@@ -32,18 +32,17 @@ V="boisversionsrai"
 accelerate launch "$EXAMPLES_DIR/train_dreambooth_lora_sdxl.py" \
   --pretrained_model_name_or_path "stabilityai/stable-diffusion-xl-base-1.0" \
   --instance_data_dir "./train_dataset/gpt_tropical" \
-  --output_dir "./final-lora-weight/adalora_gpt_tropical_rank_16" \
+  --output_dir "./final-lora-weight/adalora_gpt_tropical_rank_16_oposite" \
   --instance_prompt "a ${V} style interior" \
   --resolution 768 \
   --train_batch_size 2 \
   --gradient_accumulation_steps 2 \
-  --max_train_steps 10000 \
-  --rank 32 \
+  --max_train_steps 3000 \
+  --rank 16 \
   --mixed_precision fp16 \
   --enable_xformers_memory_efficient_attention \
   --checkpointing_steps 1000 \
-  --checkpoints_total_limit 10 \
+  --checkpoints_total_limit 3 \
   --learning_rate 1e-4 \
-  --target_rank 16 \
-  --resume_from_checkpoint "./final-lora-weight/adalora_gpt_tropical_rank_16/checkpoint-5000" \
+  --target_rank 32 \
   --use_adalora
